c07ebf71218674697e7ad43260bd1501b6d1ecf8
Sawankhanna1998 <sawan@wellnessextract.in>
2025-04-02 22:16:39 +0530

    code02-04-2025_3

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,28 +1,49 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { registerUser } from '../redux/actions/userActions';
+import { useNavigate } from "react-router-dom";
+
 const Signup = ({ switchToLogin, onClose }) => {
+  const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleSignup = (e) => {
+      e.preventDefault();
+      dispatch(registerUser({ name, email, password }));
+      navigate('/');
+  };
   return (
     <div>
-      <form className="flex flex-col gap-4 mt-4">
+      <form className="flex flex-col gap-4 mt-4" onSubmit={handleSignup}>
         <input
           type="text"
           placeholder="Full Name"
           className="border px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400"
+          value={name} 
+          onChange={(e) => setName(e.target.value)} 
+          required
         />
         <input
           type="email"
           placeholder="Email"
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          required
           className="border px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400"
         />
         <input
           type="password"
           placeholder="Password"
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          required 
           className="border px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400"
         />
         <button
           type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            onClose();
-          }}
           className="bg-black text-white px-4 py-2 rounded-md hover:bg-gray-800 transition"
         >
           Sign Up
